2e8224bcdc68188a8c973cf2f68d3c8b60519a4f
Vasugi <109725883+vasujothi@users.noreply.github.com>
2022-08-12 10:42:57 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,106 +1,25 @@
-﻿
-using System.Data.SqlClient;
+var builder = WebApplication.CreateBuilder(args);
 
-internal class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static async Task Main(string[] args)
-    {
-        StudentService? s = null;      
-        try
-        {
-            s = new StudentService();
-            s.DisplayName();
-            Console.WriteLine("TextContent");
-            await foreach (string text in s.GetStudentFileDetails())
-            {
-                Console.WriteLine(text);
-            }
-        }
-        finally
-        {
-            if (s != null)
-            {
-                s.Dispose();
-                s = null;
-            }
-        }     
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-public class StudentService : IDisposable
-{
-    private const string connectionString = "data source=localhost,1450; database=Student; user id=sa; password=vasugiS123; MultiSubnetFailover=True;";
-    private readonly SqlConnection sqlconnect = default;
-    private bool disposedValue;
-    private string[]? outPutLines = null;
+app.UseHttpsRedirection();
 
-    public StudentService()
-    {
-        sqlconnect = new SqlConnection(connectionString);
-    }
+app.UseAuthorization();
 
-    // Return the data with asynchronous execution
-    public async IAsyncEnumerable<string> GetStudentFileDetails()
-    {    
-        using (StreamReader file = new StreamReader(@"C:\Users\vsivajothi\OneDrive - Blackboard\Documents\DataBaseConnection\StudentTextFile.txt"))
-        {
-            string? ln;
-            while ((ln = await file.ReadLineAsync()) != null)
-            {
-                yield return ln;                
-            }
-            file.Close();
-        }
-    }
+app.MapControllers();
 
-    public void DisplayName()
-    {
-            SqlCommand cmd = new SqlCommand("select * from Student_Details", sqlconnect); // Creating SqlCommand objcet 
-            sqlconnect.Open(); // Opening Connection 
-            SqlDataReader stu_details = cmd.ExecuteReader(); // Executing the SQL query
-            while (stu_details.Read())
-            {
-                Console.WriteLine(stu_details["Student_Name"]);
-            }
-            Console.WriteLine("Connection Established Successfully");
-        
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposedValue)
-        {
-            if (disposing)
-            {
-                // TODO: dispose managed state (managed objects)
-            }
-            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-            if (sqlconnect != null && sqlconnect.State == System.Data.ConnectionState.Open)
-                {
-                  sqlconnect.Close();
-                  sqlconnect.Dispose();
-                }
-            if(outPutLines != null)
-            {
-                outPutLines = null;
-            }
-                // TODO: set large fields to null
-                disposedValue = true;
-        }
-    }
-
-    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-    ~StudentService()
-    {
-    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-         Dispose(disposing: false);
-    }
-
-    public void Dispose()
-    {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
-}
-
+app.Run();
